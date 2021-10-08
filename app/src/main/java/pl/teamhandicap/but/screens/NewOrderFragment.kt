2a4dc2ef49deb536053428ef897a2f5423b10b30
thorinbur <thorinbur@gmail.com>
2021-10-08 19:43:05 +0200

    added layout inflate for newOrderFargment

--- app/src/main/java/pl/teamhandicap/but/screens/NewOrderFragment.kt
+++ app/src/main/java/pl/teamhandicap/but/screens/NewOrderFragment.kt
@@ -5,6 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import pl.teamhandicap.but.R
 
 class NewOrderFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_new_order, container, false)
+    }
 }
