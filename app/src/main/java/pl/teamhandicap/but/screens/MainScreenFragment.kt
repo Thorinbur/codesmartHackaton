178b19a1abf1d106582fb9269ea00e40fbceeb6a
thorinbur <thorinbur@gmail.com>
2021-10-08 19:07:21 +0200

    added main screen layout added new order button

--- app/src/main/java/pl/teamhandicap/but/screens/MainScreenFragment.kt
+++ app/src/main/java/pl/teamhandicap/but/screens/MainScreenFragment.kt
@@ -1,7 +1,18 @@
 package pl.teamhandicap.but.screens
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import pl.teamhandicap.but.R
 
 class MainScreenFragment : Fragment() {
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main_layout, container, false)
+    }
 }
