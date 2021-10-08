13cec6cb0e1c0d952a0bf6fcac717cfe2564ced5
thorinbur <thorinbur@gmail.com>
2021-10-08 18:49:25 +0200

    fixed crash on compilation

--- app/src/main/java/pl/teamhandicap/but/screens/NewOrderFragment.kt
+++ app/src/main/java/pl/teamhandicap/but/screens/NewOrderFragment.kt
@@ -1,5 +1,9 @@
 package pl.teamhandicap.but.screens
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class NewOrderFragment : Fragment() {
