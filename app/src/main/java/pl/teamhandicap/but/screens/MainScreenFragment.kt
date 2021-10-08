683ce18485ed05373def921681b441cceaf9f257
thorinbur <thorinbur@gmail.com>
2021-10-08 19:35:37 +0200

    added navigation libs

--- app/src/main/java/pl/teamhandicap/but/screens/MainScreenFragment.kt
+++ app/src/main/java/pl/teamhandicap/but/screens/MainScreenFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main_layout.view.*
 import pl.teamhandicap.but.R
 
 class MainScreenFragment : Fragment() {
@@ -14,5 +17,12 @@ class MainScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_main_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.newOrderButton.setOnClickListener {
+          findNavController().navigate(MainScreenFragmentDirections.newOrder())
+        }
     }
 }
